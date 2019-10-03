477b2d93949fedd167896e2f02a71fd456f3cc5e
Claudio Fahey <claudio.fahey@dell.com>
2019-10-03 02:42:39 +0000

    Remove unrelated benchmark scripts
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

--- scripts/list-bookie-pvc.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-kubectl get pvc -n nautilus-pravega -o name | \
-grep ledger.*bookie | \
-xargs -i -n 1 kubectl get {} -n nautilus-pravega -o jsonpath="{.spec.volumeName}{'|'}"
-echo ""
--- scripts/pravega-benchmark.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-kubectl delete -f pravega-benchmark.yaml -n examples
-kubectl apply -f pravega-benchmark.yaml -n examples
-sleep 5s
-kubectl logs -f jobs/pravega-benchmark -n examples
