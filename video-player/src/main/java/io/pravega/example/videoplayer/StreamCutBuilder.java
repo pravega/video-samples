8ed816d7baeda5e980830e3f1e2a520e0d552e9c
Claudio Fahey <claudiofahey@gmail.com>
2020-02-21 07:51:06 -0800

    Updated documentation

--- video-player/src/main/java/io/pravega/example/videoplayer/StreamCutBuilder.java
+++ video-player/src/main/java/io/pravega/example/videoplayer/StreamCutBuilder.java
@@ -8,6 +8,14 @@ import io.pravega.client.stream.impl.StreamCutImpl;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This builds a StreamCut based on the Position of an event.
+ * The StreamCut will point to the event immediately following the event
+ * whose Position was provided. The StreamCut will also point to
+ * any unread events in other segments.
+ *
+ * TODO: Confirm that this works for multi-segment streams and through scaling events.
+ */
 public class StreamCutBuilder {
     private final Stream stream;
     private Map<Segment, Long> ownedSegmentsWithOffsets = new HashMap<>();
