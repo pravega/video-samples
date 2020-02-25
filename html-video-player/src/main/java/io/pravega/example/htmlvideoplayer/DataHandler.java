1dcc15ff6a11356fc5d95baebabfcdf8b23d9302
Claudio Fahey <claudiofahey@gmail.com>
2020-02-24 22:24:45 -0800

    HTML video player now uses etag

--- html-video-player/src/main/java/io/pravega/example/htmlvideoplayer/DataHandler.java
+++ html-video-player/src/main/java/io/pravega/example/htmlvideoplayer/DataHandler.java
@@ -1,7 +1,6 @@
 package io.pravega.example.htmlvideoplayer;
 
 import io.pravega.example.common.VideoFrame;
-import org.glassfish.grizzly.http.server.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +8,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 
 @Path("data")
 public class DataHandler {
@@ -16,10 +18,16 @@ public class DataHandler {
 
     @GET
     @Produces({"image/png"})
-    public byte[] get(@Context Request request) throws Exception {
+    public Response get(@Context Request request) throws Exception {
         try {
-            VideoFrame videoFrame = VideoPlayer.getLatestVideoFrame(0);
-            return videoFrame.data;
+            final VideoFrame videoFrame = VideoPlayer.getLatestVideoFrame(0);
+            final EntityTag etag = new EntityTag(videoFrame.timestamp.toString());
+            final Response.ResponseBuilder builder = request.evaluatePreconditions(etag);
+            if (builder != null) {
+                // Return 304 Not Modified
+                return builder.build();
+            }
+            return Response.ok(videoFrame.data).tag(etag).build();
         }
         catch (Exception e) {
             log.error(e.toString());
