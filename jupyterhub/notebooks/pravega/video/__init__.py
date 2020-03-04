785da95c038f7c3766f389b62d03ee91383002a8
Claudio Fahey <claudio.fahey@dell.com>
2020-03-04 06:55:20 +0000

    Rename pravega.video Python package
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

---
+++ jupyterhub/notebooks/pravega/video/__init__.py
@@ -0,0 +1,6 @@
+from .pravega_video import (
+    ignore_non_events,
+    opencv_image_to_mpl,
+    UnindexedStream,
+    IndexedStream,
+)
--- jupyterhub/start_jupyter_docker.sh
+++ jupyterhub/start_jupyter_docker.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
+set -ex
+
+ROOT_DIR=$(readlink -f -- "$(dirname -- "$0")/..")
+
 docker run \
 -d \
 -p 8890:8888 \
 -e JUPYTER_ENABLE_LAB=yes \
--v "$PWD":/home/jovyan/work \
+-v "${ROOT_DIR}":/home/jovyan/video-samples \
 -v /mnt:/home/jovyan/mnt \
 --name jupyter-notebook-video-samples \
 jupyter/all-spark-notebook:f646d2b2a3af \
 jupyter-lab \
 --ip=0.0.0.0 \
 --no-browser
+
+sleep 5s
+
+docker logs jupyter-notebook-video-samples
