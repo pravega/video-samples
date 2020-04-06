bb61dc1e173dd08ae9c6787b533cb7e804fb14a9
Claudio Fahey <claudio.fahey@dell.com>
2020-04-06 19:39:31 +0000

    Create Flink job to test out of memory errors
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

--- jupyterhub/start_jupyter_docker.sh
+++ jupyterhub/start_jupyter_docker.sh
@@ -7,9 +7,10 @@ docker run \
 -d \
 -p 8890:8888 \
 -e JUPYTER_ENABLE_LAB=yes \
+-e PRAVEGA_GRPC_GATEWAY_ADDRESS=${HOST_IP}:54672 \
 -v "${ROOT_DIR}":/home/jovyan/video-samples \
 --name jupyter-notebook-video-samples \
-jupyter/all-spark-notebook:f646d2b2a3af \
+jupyter/tensorflow-notebook:dc9744740e12 \
 jupyter-lab \
 --ip=0.0.0.0 \
 --no-browser
