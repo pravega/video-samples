08446cb175be97b1236c47b6d3d37f91bb72eaf0
Claudio Fahey <claudio.fahey@dell.com>
2020-04-07 06:05:06 +0000

    Video players now send compressed images to browser as stored in Pravga.
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

--- jupyterhub/notebooks/pravega/video/pravega_video.py
+++ jupyterhub/notebooks/pravega/video/pravega_video.py
@@ -227,12 +227,9 @@ class UnindexedStream(StreamBase):
         video_frame['timestamp'] = pd.to_datetime(video_frame['timestamp'], unit='ms', utc=True)
         return video_frame
 
-    def decode_video_frame_to_image_array(self, video_frame):
+    def decode_video_frame(self, video_frame):
         """Decodes the data field (JPEG or PNG) of a video frame dict."""
-        b64decoded_data = base64.b64decode(video_frame['data'])
-        encoded_image = np.frombuffer(b64decoded_data, dtype=np.uint8)
-        video_frame['image_array'] = cv2.imdecode(encoded_image, cv2.IMREAD_UNCHANGED)
-        del video_frame['data']
+        video_frame['data'] = base64.b64decode(video_frame['data'])
         return video_frame
 
     def read_video_frames(self, from_stream_cut=None, to_stream_cut=None, stop_at_tail=False, cameras=None):
@@ -243,7 +240,7 @@ class UnindexedStream(StreamBase):
             filtered_video_frames = encoded_video_frames
         else:
             filtered_video_frames = (f for f in encoded_video_frames if f['camera'] in cameras)
-        return (self.decode_video_frame_to_image_array(f) for f in filtered_video_frames)
+        return (self.decode_video_frame(f) for f in filtered_video_frames)
 
     def play_video(self, from_stream_cut=None, to_stream_cut=None, show_frame_interval=1, figsize=None, cameras=None):
         """Play a video from a Pravega stream using a Jupyter notebook."""
@@ -251,10 +248,7 @@ class UnindexedStream(StreamBase):
         for i, video_frame in enumerate(read_events):
             if i % show_frame_interval == 0:
                 IPython.display.clear_output(wait=True)
-                fig = plt.figure(figsize=figsize)
-                plt.title('frameNumber=%d, camera=%d, timestamp=%s' % (video_frame['frameNumber'], video_frame['camera'], video_frame['timestamp']))
-                plt.imshow(opencv_image_to_mpl(video_frame['image_array']));
-                plt.show()
+                IPython.display.display(IPython.display.Image(data=video_frame['data']))
 
 
 class IndexStream(StreamBase):
@@ -395,11 +389,7 @@ class IndexedStream(StreamBase):
     def read_event_to_video_frame(self, read_event):
         event_json = read_event.event
         video_frame = json.loads(event_json)
-        image_png = base64.b64decode(video_frame['data'])
-        del video_frame['data']
-        image_png_array = np.frombuffer(image_png, dtype=np.uint8)
-        image_array = cv2.imdecode(image_png_array, cv2.IMREAD_UNCHANGED)
-        video_frame['image_array'] = image_array
+        video_frame['data'] = base64.b64decode(video_frame['data'])
         video_frame['timestamp'] = pd.to_datetime(video_frame['timestamp'], unit='ms', utc=True)
         return video_frame
 
