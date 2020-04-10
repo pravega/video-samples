f5ac73583dd5f15d2cc6e4b6f69724c611386c91
Claudio Fahey <claudio.fahey@dell.com>
2020-04-10 08:30:19 +0000

    Update multi video grid job to parallelize output image encoding (incomplete)
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

---
+++ flinkprocessor/src/main/java/io/pravega/example/videoprocessor/OrderedVideoFrameCoProcessFunction.java
@@ -0,0 +1,63 @@
+package io.pravega.example.videoprocessor;
+
+import io.pravega.example.common.VideoFrame;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.co.CoProcessFunction;
+import org.apache.flink.util.Collector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.MessageFormat;
+import java.util.Optional;
+
+class OrderedVideoFrameCoProcessFunction extends CoProcessFunction<OrderedVideoFrame, OrderedVideoFrame, OrderedVideoFrame> {
+    private static Logger log = LoggerFactory.getLogger(OrderedVideoFrameCoProcessFunction.class);
+
+    private ValueState<Long> nextOutputIndexState;
+    private MapState<Long, OrderedVideoFrame> outOfOrderElementsState;
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        nextOutputIndexState = getRuntimeContext().getState(new ValueStateDescriptor<>("nextFrameNumberToEmit", Long.class));
+        outOfOrderElementsState = getRuntimeContext().getMapState(new MapStateDescriptor<>("outOfOrderElements", Long.class, OrderedVideoFrame.class));
+    }
+
+    @Override
+    public void processElement1(OrderedVideoFrame value, Context ctx, Collector<OrderedVideoFrame> out) throws Exception {
+        processElementHelper(value, ctx, out);
+    }
+
+    @Override
+    public void processElement2(OrderedVideoFrame value, Context ctx, Collector<OrderedVideoFrame> out) throws Exception {
+        processElementHelper(value, ctx, out);
+    }
+
+    private void processElementHelper(OrderedVideoFrame value, Context ctx, Collector<OrderedVideoFrame> out) throws Exception {
+        long nextOutputIndex = Optional.ofNullable(nextOutputIndexState.value()).orElse(0L);
+        if (nextOutputIndex == value.index) {
+            // We can emit this element now.
+            out.collect(value);
+            nextOutputIndex++;
+            final OrderedVideoFrame buffered = outOfOrderElementsState.get(nextOutputIndex);
+            // Check if we can emit a buffered element.
+            if (buffered != null) {
+                // Emit the buffered element now.
+                log.info("CLAUDIO Emitting buffered element {}", value);
+                out.collect(buffered);
+                outOfOrderElementsState.remove(nextOutputIndex);
+                nextOutputIndex++;
+            }
+            nextOutputIndexState.update(nextOutputIndex);
+        } else if (nextOutputIndex < value.index) {
+            // Add early element to buffer.
+            log.info("CLAUDIO Buffering early element {}", value);
+            outOfOrderElementsState.put(value.index, value);
+        } else {
+            throw new IllegalStateException(MessageFormat.format("Unexpected element order; nextOutputIndex={0}, value.index={1}", nextOutputIndex, value.index));
+        }
+    }
+}
