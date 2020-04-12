4a47259fb624783eac5ef13e0c2972135184ca9a
Claudio Fahey <claudio.fahey@dell.com>
2020-04-12 19:39:20 +0000

    Remove env-locl.sh
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

--- deploy/pks/env-local.sh
+++
@@ -1,7 +0,0 @@
-export CLUSTER_NAME=manta
-
-export PRAVEGA_CONTROLLER_URI=tcp://pravega-controller.manta.eaglemonk.intranet.nautilus-platform-dev.com:9090
-#export PRAVEGA_CONTROLLER_URI=tcp://pravega-controller.cf01.intranet.nautilus-platform-dev.com:9090
-#export PRAVEGA_CONTROLLER_URI=tls://pravega-controller.videodemo1.nautilus-lab-sulfur.com:443
-
-export PRAVEGA_SCOPE=examples
