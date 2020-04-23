65c799ba888d584c527b100f2edc08c4382ea3e7
Claudio Fahey <claudio.fahey@dell.com>
2020-04-23 16:08:24 +0000

    Change threshold in YOLO classifier to increase detected objects
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

--- common/src/main/java/io/pravega/example/tensorflow/YOLOClassifier.java
+++ common/src/main/java/io/pravega/example/tensorflow/YOLOClassifier.java
@@ -23,7 +23,7 @@ public class YOLOClassifier {
     private final static double anchors[] = {1.08,1.19,  3.42,4.41,  6.63,11.38,  9.42,5.11,  16.62,10.52};
     private final static int NUMBER_OF_BOXES = 13;
     private final static int MAX_RECOGNIZED_CLASSES = 24;
-    private final static float THRESHOLD = 0.5f;
+    private final static float THRESHOLD = 0.15f;   // Change to 0.10 to reduce detected objects.
     private final static int MAX_RESULTS = 24;
     private final static int NUMBER_OF_BOUNDING_BOXES = 5;
 
